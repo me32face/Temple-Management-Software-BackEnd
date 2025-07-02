3b4ab64026d8536fccbb331c099c0c6cfda6fda3
me32face <me32face@gmail.com>
2025-07-02 17:02:02 +0530

    Update July 2nd -Staff section update and bug fix

--- models/Staff.js
+++ models/Staff.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 
 const staffSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  role: { type: String, required: true },  // Priest, Cleaner, Manager, etc
-  contact: { type: String },
+  role: { type: String, required: true },
+  address: { type: String },
+  pincode: { type: String },
+  contact1: { type: String },
+  contact2: { type: String },
+  emergencyContact: { type: String },
   createdAt: { type: Date, default: Date.now }
 });
 
