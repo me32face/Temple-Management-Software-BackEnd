c3c1514d0f7b156beda8c5f5ec1855a55b77d0fb
me32face <me32face@gmail.com>
2025-07-03 17:46:13 +0530

    Expense section update

--- models/Expense.js
+++ models/Expense.js
@@ -4,10 +4,10 @@ const expenseSchema = new mongoose.Schema({
   date: { type: Date, default: Date.now },
   amount: { type: Number, required: true },
   purpose: { type: String, required: true },
-  category: { type: String },  // e.g. Maintenance, Salary
+  category: { type: String },
   paymentMethod: { type: String, enum: ['Cash', 'UPI'], required: true },
-  proof: { type: String },
+  note: { type: String },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('Expense', expenseSchema);
